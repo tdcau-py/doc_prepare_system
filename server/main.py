77233900920793eb5a24aad27fd40699d44492fc
Pavel <borisov.p.92@yandex.ru>
2023-03-14 16:08:59 +0300

    Created thread for client connections

--- server/main.py
+++ server/main.py
@@ -1,27 +1,26 @@
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets, QtCore, uic
 from modules.searching_inn import search_inn
 import configparser
 import socket
 import json
 
 
-class SettingsWin(QtWidgets.QMainWindow):
+class ClientConnections(QtCore.QThread):
     CONFIG_FILE = 'settings.ini'
+    config = configparser.ConfigParser()
+    config.read(CONFIG_FILE, encoding='utf8')
+    HOST = config['default']['host']
+    PORT = config['default']['port']
 
-    def __init__(self, parent=None) -> None:
+    server_status = QtCore.pyqtSignal(str)
+
+    def __init__(self, parent = None) -> None:
         super().__init__(parent)
-        gui, base = uic.loadUiType('forms/settings_form.ui')
-        self.ui = gui()
-        self.ui.setupUi(self)
-        self.setWindowTitle('Сервер системы формирования документов')
 
-        config = configparser.ConfigParser()
-        config.read(self.CONFIG_FILE, encoding='utf8')
-        self.HOST = config['default']['host']
-        self.PORT = config['default']['port']
-
-    def client_connections(self):
+    def run(self):
         """Соединение с клиентом и обмен данными"""
+        self.server_status.emit('Сервер работает.')
+
         while True:
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
                 server_socket.bind((self.HOST, self.PORT))
@@ -29,7 +28,7 @@ class SettingsWin(QtWidgets.QMainWindow):
                 connect, address = server_socket.accept()
 
                 with connect:
-                    print(f'Connected by {address}')
+                    # print(f'Connected by {address}')
 
                     while True:
                         data = connect.recv(1024)
@@ -37,6 +36,22 @@ class SettingsWin(QtWidgets.QMainWindow):
                         decode_data = json.loads(data.decode())
                         result = search_inn(decode_data)
                         connect.send(result.encode('utf8'))
+
+
+class SettingsWin(QtWidgets.QMainWindow):
+    def __init__(self, parent=None) -> None:
+        super().__init__(parent)
+        gui, base = uic.loadUiType('forms/settings_form.ui')
+        self.ui = gui()
+        self.ui.setupUi(self)
+        self.setWindowTitle('Сервер системы формирования документов')
+        
+        self.connections_thread = ClientConnections()
+        self.ui.startServer.clicked.connect(self.start_server)
+
+    def start_server(self):
+        """Соединение с клиентом и обмен данными"""
+        self.connections_thread.start()
 
 
 if __name__ == '__main__':
